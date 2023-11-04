1a1d202080c2c16a8f86e6578b81dbdd854e08dc
sitimarni <124442828+sitimarni@users.noreply.github.com>
2023-11-04 21:38:55 +0700

    Add files via upload

---
+++ module1.js
@@ -0,0 +1,5 @@
+function Tambah(angka1, angka2) {
+    return angka1 + angka2;
+  }
+  
+  module.exports = Tambah;
---
+++ module3.js
@@ -0,0 +1,6 @@
+function Bagi(angka1, angka2) {
+    return angka1 / angka2;
+  }
+  
+  module.exports = Bagi;
+  
